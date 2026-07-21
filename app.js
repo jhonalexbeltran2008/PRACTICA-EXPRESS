52d7e9c48f86544c2efbcc84378f7782f90d9e12
jhonalexbeltran2008 <jhonalexbeltran2008@gmail.com>
2026-07-21 11:13:05 -0500

    Nuevo endpoint de aplicación

--- app.js
+++ app.js
@@ -6,8 +6,14 @@ const app = express();
 const puerto = process.env.PORT || 3000;
 
 app.get("/",(rep, res)=>{
-res.send("Hola ficha 3407180")
+     res.send("Hola ficha 3407180")
 });
+
+app.get("/misaludo", (rep, res)=>{
+     res.send(`<h1>Hola, soy Jhon Beltran</h1>
+        <p>Soy aprendiz SENA</p>`)
+});
+
 
 app.listen(puerto, () => {
     console.log(`servidor funcional!! en el puerto ${puerto}`);
