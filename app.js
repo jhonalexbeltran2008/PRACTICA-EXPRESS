e7eae5cc2b2c4af8c9a827dca691eec363aa3664
jhonalexbeltran2008 <jhonalexbeltran2008@gmail.com>
2026-07-21 10:38:51 -0500

    Instalación de la aplicación

---
+++ app.js
@@ -0,0 +1,14 @@
+import express from "express";
+import{configDotenv} from "dotenv"
+configDotenv()
+
+const app = express();
+const puerto = process.env.PORT || 3000;
+
+app.get("/",(rep, res)=>{
+res.send("Hola ficha 3407180")
+});
+
+app.listen(puerto, () => {
+    console.log('servidor funcional!! en el puerto ${puerto}');
+});
