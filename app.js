3784149c66ca69632374a9ed76073e32b8a91dd5
jhonalexbeltran2008 <jhonalexbeltran2008@gmail.com>
2026-07-28 09:52:07 -0500

    Nuevos endpoints .get 28/07

--- app.js
+++ app.js
@@ -11,22 +11,40 @@ app.get("/saludo/:ficha", (req, res) => {
         <p>Soy de la ficha: ${ficha}</p>`);
 });
 
-
-
-app.get("/misaludo", (rep, res)=>{
+app.get("/misaludo", (req, res)=>{
      res.send(`<h1>Hola, soy Jhon Beltran</h1>
         <p>Soy aprendiz SENA</p>`)
 });
 
-app.get("/clientes/:id", (rep, res) => {
+app.get("/clientes/:id", (req, res) => {
      const id = req.params.id;
      res.send(`<h1>Clientes</h1>
         <p>Soy el cliente con ID ${id}</p>`);
 });
 
-
-
 app.listen(puerto, () => {
     console.log(`SERVIDOR http://localhoost:${puerto}
         http:127.0.0.1:${puerto}`)
+});
+
+// 28/07
+
+app.get("/productos/:categoria", (req, res) => {
+     const categoria = req.params.categoria;
+      res.send(`<h1>Categoria</h1>
+        <p> Producto de la categoria ${categoria} </p>`)
+});
+
+
+app.get("/productos/:categoria/:id", (req, res) => {
+     const categoria = req.params.categoria;
+     const id = req.params.id;
+     res.send(`<h1>Categoria con ID</h1>        
+        <p>Producto de la categoria ${categoria} con ID ${id} </p>`)
+});
+ 
+app.get("/Libros/:isbn", (req, res) => {
+     const isbn = req.params.isbn;
+     res.send(`<h1>Libros</h1>
+        <p>Libro con isbn ${isbn}</p>`)
 });
